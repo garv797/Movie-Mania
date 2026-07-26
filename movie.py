0036d0c500c677650105eda8126173d1aa83a3ab
unknown <garvmanchanda14@gmail.com>
2026-07-26 18:52:47 +0530

    Update requirements

--- movie.py
+++ movie.py
@@ -1,5 +1,4 @@
-import os
-from turtle import st
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
